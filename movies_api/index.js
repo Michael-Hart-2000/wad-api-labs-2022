3f3264cfa08e6bd1b7b446a76e1fc4e51e4a1d49
Your Name <you@example.com>
2022-05-13 11:56:32 +0100

    Topic 10 - Passport

--- movies_api/index.js
+++ movies_api/index.js
@@ -5,7 +5,7 @@ import './db';
 import './seedData'
 import usersRouter from './api/users';
 import session from 'express-session';
-import authenticate from './authenticate';
+import passport from './authenticate';
 
 dotenv.config();
 
@@ -29,12 +29,9 @@ app.use('/api/users', usersRouter);
 
 app.use(errHandler);
 
-//session middleware
-app.use(session({
-    secret: 'ilikecake',
-    resave: true,
-    saveUninitialized: true
-  }));
+app.use(passport.initialize());
+
+app.use('/api/movies', passport.authenticate('jwt', {session: false}), moviesRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
